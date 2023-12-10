bca5fbe19429c6fdfb50b8bf0207fc9070cdcfff
James Stout <james.wolf.stout@gmail.com>
2023-12-09 21:49:38 -0800

    Separate newline and CRLF tests.

--- test/test.js
+++ test/test.js
@@ -6,6 +6,11 @@ parser.setLanguage(Talon);
 
 describe('Parser', function () {
     it('should strip newlines from comments', function () {
+        const sourceCode = '# Comment\n-';
+        const tree = parser.parse(sourceCode);
+        expect(tree.rootNode.child(0).text).toEqual('# Comment');
+    });
+    it('should strip CRLFs from comments', function () {
         const sourceCode = '# Comment\r\n-';
         const tree = parser.parse(sourceCode);
         expect(tree.rootNode.child(0).text).toEqual('# Comment');
